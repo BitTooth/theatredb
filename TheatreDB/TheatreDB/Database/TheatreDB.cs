43905ca702677338fbebfc31a8e8a1b0c108b228
lexa107 <lexa107@gmail.com>
2014-05-17 15:14:20 +0300

    added select by genre

--- TheatreDB/TheatreDB/Database/TheatreDB.cs
+++ TheatreDB/TheatreDB/Database/TheatreDB.cs
@@ -28,6 +28,19 @@ namespace TheatreDB.Database
             }
         }
 
+        // To use this method you must use "SELECT * FROM спектакль ..."
+        private Play readPlayData(MySqlDataReader rdr)
+        {
+            Play pl = new Play();
+            pl.name = rdr.GetString(0);
+            pl.story = rdr.GetString(1);
+            pl.year = rdr.GetUInt32(2);
+            pl.actorsNum = rdr.GetUInt32(3);
+            pl.discount = rdr.GetUInt32(4);
+            pl.ID = rdr.GetUInt32(5);
+            return pl;
+        }
+
         public List<Play> getPlayList()
         {
             List<Play> list = new List<Play>();
@@ -38,23 +51,34 @@ namespace TheatreDB.Database
 
             while (rdr.Read())
             {
-                Play pl = new Play();
-                pl.name         = rdr.GetString(0);
-                pl.story        = rdr.GetString(1);
-                pl.year         = rdr.GetUInt32(2);
-                pl.actorsNum    = rdr.GetUInt32(3);
-                pl.discount     = rdr.GetUInt32(4);
-                pl.ID           = rdr.GetUInt32(5);
-                list.Add(pl);
+                list.Add(readPlayData(rdr));
             }
 
             return list;
         }
 
-//         public List<Play> getPlayListByGenre()
-//         {
-// 
-//         }
+
+        public List<Play> getPlayListByGenre(string name)
+        {
+            List<Play> list = new List<Play>();
+
+            MySqlDataReader rdr = null;
+            string stm = string.Format(@"SELECT *  FROM спектакль  where id_спектакля = (SELECT id_жанра FROM жанры WHERE название_жанра = '{0}')", name);
+            MySqlCommand cmd = new MySqlCommand(stm, connection);
+            rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                list.Add(readPlayData(rdr));
+            }
+
+            return list;
+        }
+
+        public List<Play> getPlayListByGenre(Genre genre)
+        {
+            return getPlayListByGenre(genre.name);
+        }
 
         private MySqlConnection connection;
     }
