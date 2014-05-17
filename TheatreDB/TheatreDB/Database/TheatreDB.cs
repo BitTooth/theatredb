0b56843f37dc1946a2a812cef50e36c5ed0897cd
lexa107 <lexa107@gmail.com>
2014-05-17 13:49:26 +0300

    added object class defenitions

---
+++ TheatreDB/TheatreDB/Database/TheatreDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace TheatreDB.Database
+{
+    class TheatreDB
+    {
+        public void connect()
+        {
+            string cs = @"server:localhost;userid=root;password=123;database='театр'";
+
+
+            try
+            {
+                connection = new MySqlConnection(cs);
+                connection.Open();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error connecting database\n Error: " + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private MySqlConnection connection;
+    }
+}
