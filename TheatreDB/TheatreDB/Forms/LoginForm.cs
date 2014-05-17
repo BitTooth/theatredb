f077548352fbf62d5bb736eb05212a61f838e924
unknown <a_ulitenok@by1-ww-830.corp.wargaming.local>
2014-05-17 15:34:24 +0300

    Auth functionality

--- TheatreDB/TheatreDB/Forms/LoginForm.cs
+++ TheatreDB/TheatreDB/Forms/LoginForm.cs
@@ -16,11 +16,47 @@ namespace TheatreDB.Forms
         public LoginForm()
         {
             InitializeComponent();
+
+            admin = new LoginPassPair("root", "123");
         }
 
         private void authButton_Click(object sender, EventArgs e)
         {
-            
+            LoginPassPair loginPassPair = new LoginPassPair(loginTextBox.Text, passwordTextBox.Text);
+
+            if (loginPassPair == admin)
+            {
+                
+            }
+            else
+            {
+
+            }
         }
+
+        struct LoginPassPair
+        {
+            public LoginPassPair(string l, string p)
+            {
+                login = l;
+                password = p;
+            }
+
+            public static bool operator ==(LoginPassPair a, LoginPassPair b)
+            {
+                if (a.login == b.login && a.password == b.password)
+                    return true;
+
+                return false;
+            }
+
+            public static bool operator !=(LoginPassPair a, LoginPassPair b)
+            {
+                return !(a == b);
+            }
+
+            string login, password;
+        }
+        private LoginPassPair admin; 
     }
 }
